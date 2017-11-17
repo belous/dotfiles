bb109d170267fdddbbbbca7a94c258d3978457b0
Sergey Belous <belous.ios@gmail.com>
2017-11-17 17:20:23 +0400

    Improved automation script and added .gitignore

--- xcode/automate.sh
+++ xcode/automate.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Custom Key Bindings
-ln -n key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
+ln -s $(pwd)/key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
 
 # Code snippets
-mkdir -p ~/Library/Developer/Xcode/UserData/CodeSnippets
-cp -r code-snippets/. ~/Library/Developer/Xcode/UserData/CodeSnippets
+ln -s $(pwd)/code-snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
