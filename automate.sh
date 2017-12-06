3877e2521e93c4a80bf31cb64c2126b09a376664
Sergey Belous <belous.ios@gmail.com>
2017-12-06 17:13:47 +0400

    Added gitconfig and gitignore files and updated automate.sh

---
+++ automate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Custom Key Bindings
+ln -s $(pwd)/xcode/key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
+
+# Code snippets
+ln -s $(pwd)/xcode/code-snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+# git
+ln -s $(pwd)/gitignore_global ~/.gitignore_global
+ln -s $(pwd)/gitconfig ~/.gitconfig
--- xcode/automate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Custom Key Bindings
-ln -s $(pwd)/key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
-
-# Code snippets
-ln -s $(pwd)/code-snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
