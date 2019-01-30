7950d91ca0fcb1be3c7273239f86ff2f23b9ff49
Sergey Belous <belous.ios@gmail.com>
2019-01-30 13:39:25 +0400

    Update fish.config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,3 +14,15 @@ set -gx PATH $PATH $GOPATH/bin
 # set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
 
 set -x PATH $HOME/.fastlane/bin $PATH
+
+# ==> node@8
+# node@8 is keg-only, which means it was not symlinked into /usr/local,
+# because this is an alternate version of another formula.
+
+# If you need to have node@8 first in your PATH run:
+#   echo 'set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths' >> ~/.config/fish/config.fish
+
+# For compilers to find node@8 you may need to set:
+#   set -gx LDFLAGS "-L/usr/local/opt/node@8/lib"
+#   set -gx CPPFLAGS "-I/usr/local/opt/node@8/include"
+set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths
