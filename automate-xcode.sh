629b0ac597ba817ebe74e1e8ada00c9597bae884
Sergei Belous <belous.ios@gmail.com>
2021-10-14 17:50:05 +0200

    Update .dotfiles

---
+++ automate-xcode.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# create link for my_awesome_key_bindings_layout in ~/Library/Developer/Xcode/UserData/KeyBindings
+ln -s $(pwd)/xcode/key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
+
+# create a FontAndColorThemes folder for xcode
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+# create link for Mariana.xccolortheme in FontAndColorThemes folder
+ln -s $(pwd)/xcode/themes/Mariana.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Mariana.xccolortheme
--- automate.sh
+++ automate.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Custom Key Bindings
-ln -s $(pwd)/xcode/key-bindings/my_awesome_key_bindings_layout.idekeybindings ~/Library/Developer/Xcode/UserData/KeyBindings/My\ Awesome\ Key\ Bindings\ Layout.idekeybindings
-
-# Code snippets
-ln -s $(pwd)/xcode/code-snippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-
 # git
 ln -s $(pwd)/gitignore_global ~/.gitignore_global
 ln -s $(pwd)/gitconfig ~/.gitconfig
@@ -17,11 +11,3 @@ ln -s $(pwd)/config/fish ~/.config/fish
 # multi language spelling check
 # https://geektimes.ru/post/80123/
 ln -s $(pwd)/ru ~/Library/Spelling/ru
-
-# create folder for custom themes for Xcode
-mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-
-# go
-# install go: `brew install go`
-# re-export env paths?
-# mkdir -p $GOPATH $GOPATH/src $GOPATH/pkg $GOPATH/bin
